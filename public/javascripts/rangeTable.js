16cc33643a5314587dcb8d542dfd90bf87b0836e
Andrew Sayman <andrew.sayman@gmail.com>
2014-05-31 14:46:56 -0400

    Rules for weapon ranges

---
+++ public/javascripts/rangeTable.js
@@ -0,0 +1,167 @@
+RANGE_TABLE = {
+    "name": "range table",
+    "reference": "SR5:185",
+    "ranges": [
+        {
+            "name": "Taser",
+            "short": "0-5",
+            "medium": "6-10",
+            "long": "11-15",
+            "extreme": "16-20"
+        },
+        {
+            "name": "Hold-Out Pistol",
+            "short": "0-5",
+            "medium": "6-15",
+            "long": "16-30",
+            "extreme": "31-50"
+        },
+        {
+            "name": "Light Pistol",
+            "short": "0-5",
+            "medium": "6-15",
+            "long": "16-30",
+            "extreme": "31-50"
+        },
+        {
+            "name": "Heavy Pistol",
+            "short": "0-5",
+            "medium": "6-20",
+            "long": "21-40",
+            "extreme": "41-60"
+        },
+        {
+            "name": "Machine Pistol",
+            "short": "0-5",
+            "medium": "6-15",
+            "long": "16-30",
+            "extreme": "31-50"
+        },
+        {
+            "name": "SMG",
+            "short": "0-10",
+            "medium": "11-40",
+            "long": "41-80",
+            "extreme": "81-150"
+        },
+        {
+            "name": "Assault Rifle",
+            "short": "0-25",
+            "medium": "26-150",
+            "long": "151-350",
+            "extreme": "351-550"
+        },
+        {
+            "name": "Shotgun (flechette)",
+            "short": "0-15",
+            "medium": "16-30",
+            "long": "31-45",
+            "extreme": "45-60"
+        },
+        {
+            "name": "Shotgun (slug)",
+            "short": "0-10",
+            "medium": "11-40",
+            "long": "41-80",
+            "extreme": "81-150"
+        },
+        {
+            "name": "Sniper Rifle",
+            "short": "0-50",
+            "medium": "51-350",
+            "long": "351-800",
+            "extreme": "801-1,500"
+        },
+        {
+            "name": "Light Machinegun",
+            "short": "0-25",
+            "medium": "26-200",
+            "long": "201-400",
+            "extreme": "401-800"
+        },
+        {
+            "name": "Medium/Heavy Machinegun",
+            "short": "0-40",
+            "medium": "41-250",
+            "long": "251-750",
+            "extreme": "751-1,200"
+        },
+        {
+            "name": "Assault Cannon",
+            "short": "0-50",
+            "medium": "51-300",
+            "long": "301-750",
+            "extreme": "751-1,500"
+        },
+        {
+            "name": "Grenade Launcher",
+            "short": "5-50*",
+            "medium": "51-100",
+            "long": "101-150",
+            "extreme": "151-500"
+        },
+        {
+            "name": "Missile Launcher",
+            "short": "20-70*",
+            "medium": "71-150",
+            "long": "151-450",
+            "extreme": "451-1,500"
+        },
+        {
+            "name": "Light Crossbow",
+            "short": "0-6",
+            "medium": "7-24",
+            "long": "25-60",
+            "extreme": "61-120"
+        },
+        {
+            "name": "Medium Crossbow",
+            "short": "0-9",
+            "medium": "10-36",
+            "long": "37-90",
+            "extreme": "91-150"
+        },
+        {
+            "name": "Heavy Crossbow",
+            "short": "0-15",
+            "medium": "16-45",
+            "long": "46-120",
+            "extreme": "121-180"
+        },
+        {
+            "name": "Bow",
+            "short": "0-STR",
+            "medium": "To STR x 10",
+            "long": "To STR x 30",
+            "extreme": "To STR x 60"
+        },
+        {
+            "name": "Thrown Knife",
+            "short": "0-STR",
+            "medium": "To STR x 2",
+            "long": "To STR x 3",
+            "extreme": "To STR x 5"
+        },
+        {
+            "name": "Shuriken",
+            "short": "0-STR",
+            "medium": "To STR x 2",
+            "long": "To STR x 5",
+            "extreme": "To STR x 7"
+        },
+        {
+            "name": "Standard Grenade",
+            "short": "0-STR x 2",
+            "medium": "To STR x 4",
+            "long": "To STR x 6",
+            "extreme": "To STR x 10"
+        },
+        {
+            "name": "Aerodynamic Grenade",
+            "short": "0-STR x 2",
+            "medium": "To STR x 4",
+            "long": "To STR x 8",
+            "extreme": "To STR x 15"
+        }
+    ]
+}
