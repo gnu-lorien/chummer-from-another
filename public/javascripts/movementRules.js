83706bad09b73753b24a9d16a74ac9fbcea9303b
Andrew Sayman <andrew.sayman@gmail.com>
2014-05-31 11:51:21 -0400

    Derived movement rules reference

---
+++ public/javascripts/movementRules.js
@@ -0,0 +1,14 @@
+MOVEMENT_RULES = {
+    "reference": "SR5:162",
+    "rates": {
+        "walk": "Agility * 2",
+        "run": "Agility * 4"
+    },
+    "sprintIncrease": {
+        "Dwarf": 1,
+        "Troll": 1,
+        "Human": 2,
+        "Elf": 2,
+        "Ork": 2
+    }
+}
