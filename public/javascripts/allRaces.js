77c06bc39ba13f5a0c2e1cfbea5bf50affe6fe7f
eador <caslate@gmail.com>
2014-05-31 01:55:33 -0400

    Added a races reference file that covers the core races along with starting attributes, maximum attributes, enchanced maximum attributs, racial qualities, and lifestyle multipliers.

---
+++ public/javascripts/allRaces.js
@@ -0,0 +1,82 @@
+ALL_RACES = [
+  {
+    "name":"dwarf",
+    "attributes": {
+      "agility": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "body": {"start":3, "naturalMaximum":8, "enhancedMaximum":12},
+      "charisma": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "edge": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "intuition": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "logic": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},      
+      "reaction": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},
+      "strength": {"start":3, "naturalMaximum":8, "enhancedMaximum":12},
+      "willpower": {"start":2, "naturalMaximum":7, "enhancedMaximum":10}
+    },
+    "qualities": ["toxin resistance"],
+    "lifestyleMultiplier":1.2
+  },
+  {
+    "name":"elf",
+    "attributes": {
+      "agility": {"start":2, "naturalMaximum":7, "enhancedMaximum":10},
+      "body": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "charisma": {"start":3, "naturalMaximum":8, "enhancedMaximum":12},
+      "edge": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "intuition": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "logic": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},      
+      "reaction": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "strength": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "willpower": {"start":1, "naturalMaximum":6, "enhancedMaximum":9}
+    },
+    "qualities": ["low-light vision"],
+    "lifestyleMultiplier":1
+  },
+  {
+    "name":"human",
+    "attributes": {
+      "agility": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "body": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "charisma": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "edge": {"start":2, "naturalMaximum":7, "enhancedMaximum":10},
+      "intuition": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "logic": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},      
+      "reaction": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "strength": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "willpower": {"start":1, "naturalMaximum":6, "enhancedMaximum":9}
+    },
+    "qualities": [],
+    "lifestyleMultiplier":1
+  },
+  {
+    "name":"ork",
+    "attributes": {
+      "agility": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "body": {"start":4, "naturalMaximum":9, "enhancedMaximum":13},
+      "charisma": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},
+      "edge": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "intuition": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "logic": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},      
+      "reaction": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "strength": {"start":3, "naturalMaximum":8, "enhancedMaximum":12},
+      "willpower": {"start":1, "naturalMaximum":6, "enhancedMaximum":9}
+    },
+    "qualities": ["low-light vision"],
+    "lifestyleMultiplier":1
+  },
+  {
+    "name":"troll",
+    "attributes": {
+      "agility": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},
+      "body": {"start":5, "naturalMaximum":10, "enhancedMaximum":15},
+      "charisma": {"start":1, "naturalMaximum":4, "enhancedMaximum":6},
+      "edge": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "intuition": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},
+      "logic": {"start":1, "naturalMaximum":5, "enhancedMaximum":7},      
+      "reaction": {"start":1, "naturalMaximum":6, "enhancedMaximum":9},
+      "strength": {"start":5, "naturalMaximum":10, "enhancedMaximum":15},
+      "willpower": {"start":1, "naturalMaximum":6, "enhancedMaximum":9}
+    },
+    "qualities": ["thermographic vision", "reach(1)", "armor(1)"],
+    "lifestyleMultiplier":2
+  }
+]
